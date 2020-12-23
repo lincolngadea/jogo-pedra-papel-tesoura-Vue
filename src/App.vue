266b381fc984c985191968b25bb81b172c178438
Lincoln Gadea <lincoln@glinc.com.br>
2020-12-23 15:09:42 -0300

    chore: download frontmentor, install and configuration of vuejs

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="app">
+		<div id="nav" />
+		<RouterView />
+	</div>
+</template>
+
+<style lang="scss">
+	@import './styles/reset';
+
+	#app {
+		font-family: 'Barlow Semi Condensed', Helvetica, Arial, sans-serif;
+	}
+</style>
