f6eccbc361fd1da411ff0ea2df39023e9dfbfc63
Lincoln Gadea <lincoln@glinc.com.br>
2020-12-23 15:38:40 -0300

    chor: create mouse animation and background styles for mouse animation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template>
-	<div id="app">
+	<div id="app"
+		:style="backgroundGradient"
+		@mousemove="redefinePositions"
+		@mouseleave ="resetPositions">
 		<div id="nav" />
 		<RouterView />
 	</div>
 </template>
 
+<script>
+	import {bgColorFrom, bgColorTo } from './styles/variables.scss';
+	export default {
+		name: 'App',
+		data() {
+			return {
+				gradientX: '50%',
+				gradientY: '0%',
+			};
+		},
+		computed:{
+			backgroundGradient(){
+				return {
+					background: `radial-gradient(circle at
+																				${this.gradientX} ${this.gradientY},
+																				${bgColorFrom},
+																				${bgColorTo})`,
+				};
+			},
+		},
+		methods: {
+			redefinePositions({ clientX, clientY }) {
+				this.gradientX = `${clientX}px`;
+				this.gradientY = `${clientY}px`;
+			},
+			resetPositions(){
+				this.gradientX = '50%';
+				this.gradientY = '0%';
+			}
+		},
+};
+</script>
+
 <style lang="scss">
 	@import './styles/reset';
 
 	#app {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
 		font-family: 'Barlow Semi Condensed', Helvetica, Arial, sans-serif;
 	}
 </style>
