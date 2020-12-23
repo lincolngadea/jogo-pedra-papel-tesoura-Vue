7600f0e8d48e503dd8ba3313e67311bfe8bc4718
Lincoln Gadea <lincoln@glinc.com.br>
2020-12-23 16:49:24 -0300

    chore: create HEADER and stylization

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,19 @@
 		:style="backgroundGradient"
 		@mousemove="redefinePositions"
 		@mouseleave ="resetPositions">
-		<div id="nav" />
+		<Header />
 		<RouterView />
 	</div>
 </template>
 
 <script>
+	import Header from '@/components/Header/Header';
 	import {bgColorFrom, bgColorTo } from './styles/variables.scss';
 	export default {
 		name: 'App',
+		components:{
+			Header,
+		},
 		data() {
 			return {
 				gradientX: '50%',
